57a950260922f0f5cc1f7829f137ca06b3563b37
AbderrahmaneNamat <abderhmannamat18@gmail.com>
2025-11-15 00:29:19 +0100

    AdminChanges

--- MiniBank.java
+++ MiniBank.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOError;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -10,6 +11,7 @@ import java.time.chrono.IsoChronology;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 import login.Login;
 import members.Members;
 import register.Register;
@@ -90,31 +92,73 @@ class Main {
         }
         private static void seeAllMembers(){
             ArrayList<Members> members=loadMembers();
+            if (members == null || members.isEmpty()){
+                System.out.println("Aucune Utilisateur est enregistrer");
+            }
             int i=1;
             for(Members mem:members){
                 System.out.printf("%d - UserName: %s ; Password: %s \n",i,mem.getMember(),mem.getMemberPass());
                 i++;
             }
         }
-        //======== PART OF MEMBER DETAILS ( DEPOSIT,WITHDRAW)=========
+        //======== PART OF MEMBER DETAILS ( DEPOSIT,WITHDRAW) ===========
 
-        private static void checkIntegerInputValidation(int val){
-            try{
-                val=scanner.nextInt();
-                scanner.nextLine();
-            }catch(InputMismatchException e){
-                System.out.println("Make Sure To Enter Integer Number");
+        private static int checkIntegerInputValidation(){
+            while(true){
+                try{
+                    int val=scanner.nextInt();
+                    scanner.nextLine();
+                    return val;
+                }catch(InputMismatchException e){
+                    System.out.println("Make Sure To Enter Integer Number");
+                    scanner.nextLine();
+                }
             }
+
         }
         private static void adminCheckMember(){
-            System.out.println("lol");
+            members=loadMembers();
+            System.out.println("Enter name:");
+            String username=scanner.nextLine();
+            int i=0;
+            for(Members mem:members){
+                i++;
+                if(username.equals(mem.getMember())){
+                    System.out.println("User Exist With the Following info");
+                    System.out.printf("%d - UserName: %s ; Password: %s \n",i,mem.getMember(),mem.getMemberPass());
+                }else{
+                    System.out.println("User Not founded");
+                }
+            }    
+        }
+    private static void deleteuser() {
+        members = loadMembers();
+
+        System.out.print("Enter username to delete: ");
+        String username = scanner.nextLine();
+
+         boolean removed = members.removeIf(m -> m.getMember().equalsIgnoreCase(username));
+
+         if (removed) {
+             System.out.println("User Deleted Successfully.");
+        } else {
+            System.out.println("User Not Found.");
+        }
+
+        usersave(members);
+    }
+        private static void usersave(ArrayList<Members> members){
+            try(ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(FILE_NAME));){
+                oos.writeObject(members);
+            }catch(IOException e){
+                System.out.println("File Not Saved");
+            }
+               
         }
         @SuppressWarnings("unchecked")
         public static ArrayList<Members> loadMembers(){
             try (ObjectInputStream ois=new ObjectInputStream(new FileInputStream(FILE_NAME))) {
-                  
-                    return (ArrayList<Members>) ois.readObject();
-
+                return (ArrayList<Members>) ois.readObject();
             } catch (Exception e) {
                     System.out.println("Members Not  Loaded");
                     return new ArrayList<>();
